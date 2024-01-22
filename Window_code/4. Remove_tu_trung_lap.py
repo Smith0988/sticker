10fba3761830fae6081bc85dc1b97cc1f9d18102
Smith0988 <139751138+Smith0988@users.noreply.github.com>
2024-01-22 07:18:40 +0700

    tttt

--- Window_code/4. Remove_tu_trung_lap.py
+++ Window_code/4. Remove_tu_trung_lap.py
@@ -1,5 +1,5 @@
 # Tạo một list chứa các cụm từ cần kiểm tra
-phrases_to_check = ["you won't make"]
+phrases_to_check = ["but History"]
 
 # Tạo một tập hợp để theo dõi các câu đã xuất hiện
 seen_phrases = set()
---
+++ base_data/rubble web/KEYWORK/17. I am not a/remove dup.py
@@ -0,0 +1,15 @@
+def remove_duplicates(input_file, output_file):
+    # Mở file đầu vào để đọc
+    with open(input_file, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    # Loại bỏ các câu trùng lặp
+    unique_lines = list(set(line.lower() for line in lines))
+
+    # Mở file đầu ra để ghi
+    with open(output_file, 'w', encoding='utf-8') as file:
+        # Ghi các câu không trùng lặp vào file mới
+        file.writelines(unique_lines)
+
+# Gọi hàm và truyền tên file đầu vào và tên file đầu ra
+remove_duplicates('unmatched_data.txt', 'unique_data_no_duplicates.txt')
